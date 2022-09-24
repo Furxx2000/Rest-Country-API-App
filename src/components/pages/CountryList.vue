f5cd0b10ac9e6aa7e87af9387c0035760ab87d7c
furxx2000 <furxx2000@gmail.com>
2022-09-25 00:32:53 +0800

    Add Base filter component

--- src/components/pages/CountryList.vue
+++ src/components/pages/CountryList.vue
@@ -3,13 +3,15 @@ import {} from "./CountryList.vue";
 </script>
 
 <template>
-  <main>
+  <main class="grid">
     <BaseSearch />
+    <BaseFilter />
   </main>
 </template>
 
 <style scoped>
 main {
   padding: 1.5rem 1rem;
+  --gap: 2.5rem;
 }
 </style>
