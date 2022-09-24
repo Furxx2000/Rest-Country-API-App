37fb9aac8cf6ccb62e1b90ad21df24ea4f9776b6
furxx2000 <furxx2000@gmail.com>
2022-09-25 00:33:02 +0800

    Create new component

---
+++ src/components/UI/BaseFilter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <input class="fs-input fw-400 text-dark-blue-3" type="text" readonly />
+    <ul></ul>
+  </div>
+</template>
+
+<style scoped>
+input {
+  width: 200px;
+  padding: 14px 24px;
+}
+
+input:focus {
+  outline: none;
+}
+</style>
