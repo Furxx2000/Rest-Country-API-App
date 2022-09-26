1a99bca01ccb7410f42cfdf84fbdb91fe5f08e48
furxx2000 <furxx2000@gmail.com>
2022-09-27 00:02:06 +0800

    Import icon and styling input

--- src/components/UI/BaseFilter.vue
+++ src/components/UI/BaseFilter.vue
@@ -1,18 +1,46 @@
 <script setup lang="ts"></script>
 <template>
-  <div>
-    <input class="fs-input fw-400 text-dark-blue-3" type="text" readonly />
-    <ul></ul>
+  <div class="input-filter">
+    <input
+      class="fs-input fw-400 text-dark-blue-3"
+      type="text"
+      placeholder="Filter by Region"
+      readonly
+    />
+
+    <button class="icon-angle-down">
+      <font-awesome-icon
+        icon="fa-solid fa-angle-down"
+        color="hsl(var(--clr-dark-blue-3))"
+        size="xs"
+      />
+    </button>
   </div>
 </template>
 
 <style scoped>
-input {
+.input-filter {
+  position: relative;
   width: 200px;
+}
+
+.input-filter input {
+  width: 100%;
   padding: 14px 24px;
 }
 
-input:focus {
+.input-filter input:focus {
   outline: none;
+}
+
+.icon-angle-down {
+  position: absolute;
+  right: 19px;
+  top: 50%;
+  translate: 0 -50%;
+}
+
+::placeholder {
+  color: hsl(var(--clr-dark-blue-3));
 }
 </style>
--- src/components/UI/BaseSearch.vue
+++ src/components/UI/BaseSearch.vue
@@ -2,19 +2,40 @@
 import {} from "./BaseSearch.vue";
 </script>
 <template>
-  <input
-    class="fs-input fw-400 text-ultra-light-gray"
-    type="text"
-    placeholder="Search for a country..."
-  />
+  <div class="input-search">
+    <button class="icon-search">
+      <font-awesome-icon
+        icon="fa-solid fa-magnifying-glass"
+        color="hsl(var(--clr-ultra-light-gray))"
+      />
+    </button>
+
+    <input
+      class="fs-input fw-400 text-ultra-light-gray"
+      type="text"
+      placeholder="Search for a country..."
+    />
+  </div>
 </template>
 <style scoped>
-input {
-  width: 100%;
-  padding: 14px 2rem;
+.input-search {
+  position: relative;
 }
 
-input:focus {
+.input-search input {
+  width: 100%;
+  padding: 14px 2rem;
+  padding-left: 74px;
+}
+
+.input-search input:focus {
   outline: none;
+}
+
+.icon-search {
+  position: absolute;
+  top: 50%;
+  left: 2rem;
+  translate: 0 -50%;
 }
 </style>
