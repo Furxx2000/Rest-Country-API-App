0a9113ac49ebd925d1f81cc6bf5f6f94c192c26d
furxx2000 <furxx2000@gmail.com>
2022-09-27 00:01:23 +0800

    Setting font awesome

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 import { createApp } from "vue";
-import "./css/style.css";
 import App from "./App.vue";
+import "./css/style.css";
 
-createApp(App).mount("#app");
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
+import {
+  faMagnifyingGlass,
+  faAngleDown,
+} from "@fortawesome/free-solid-svg-icons";
+
+/* Add icons to the library */
+library.add(faMoon, faMagnifyingGlass, faAngleDown);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
