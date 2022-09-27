55babb8da84bec290660f381c8a62ec3ef89e7b9
furxx2000 <furxx2000@gmail.com>
2022-09-27 22:43:53 +0800

    Complete BaseSearch component

--- src/components/UI/BaseSearch.vue
+++ src/components/UI/BaseSearch.vue
@@ -2,8 +2,8 @@
 import {} from "./BaseSearch.vue";
 </script>
 <template>
-  <div class="input-search">
-    <button class="icon-search">
+  <div class="form-control">
+    <button class="icon search">
       <font-awesome-icon
         icon="fa-solid fa-magnifying-glass"
         color="hsl(var(--clr-ultra-light-gray))"
@@ -11,31 +11,19 @@ import {} from "./BaseSearch.vue";
     </button>
 
     <input
-      class="fs-input fw-400 text-ultra-light-gray"
+      class="fs-input fw-400 card-style text-ultra-light-gray"
       type="text"
       placeholder="Search for a country..."
     />
   </div>
 </template>
 <style scoped>
-.input-search {
-  position: relative;
-}
-
-.input-search input {
-  width: 100%;
+input {
   padding: 14px 2rem;
   padding-left: 74px;
 }
 
-.input-search input:focus {
-  outline: none;
-}
-
-.icon-search {
-  position: absolute;
-  top: 50%;
+.search {
   left: 2rem;
-  translate: 0 -50%;
 }
 </style>
