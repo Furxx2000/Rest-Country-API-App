cfdf83d9d9d754d1797bcd6e37e9948b7fa39cce
furxx2000 <furxx2000@gmail.com>
2022-09-27 22:43:31 +0800

    Complete BaseFilter component

--- src/components/UI/BaseFilter.vue
+++ src/components/UI/BaseFilter.vue
@@ -1,46 +1,65 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+
+const isOpenFilter = ref(false);
+
+function toggleFilter() {
+  isOpenFilter.value = !isOpenFilter.value;
+}
+</script>
 <template>
-  <div class="input-filter">
+  <div @click="toggleFilter" class="form-control input-filter">
     <input
-      class="fs-input fw-400 text-dark-blue-3"
+      class="fs-input fw-400 card-style text-dark-blue-3"
       type="text"
       placeholder="Filter by Region"
       readonly
     />
 
-    <button class="icon-angle-down">
+    <button class="icon angle-down">
       <font-awesome-icon
         icon="fa-solid fa-angle-down"
         color="hsl(var(--clr-dark-blue-3))"
         size="xs"
       />
     </button>
+
+    <ul v-if="isOpenFilter" class="bg-white card-style">
+      <li>Africa</li>
+      <li>America</li>
+      <li>Asia</li>
+      <li>Europe</li>
+      <li>Oceania</li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
 .input-filter {
-  position: relative;
   width: 200px;
 }
 
-.input-filter input {
-  width: 100%;
+input {
   padding: 14px 24px;
 }
 
-.input-filter input:focus {
-  outline: none;
-}
-
-.icon-angle-down {
-  position: absolute;
+.angle-down {
   right: 19px;
-  top: 50%;
-  translate: 0 -50%;
 }
 
 ::placeholder {
   color: hsl(var(--clr-dark-blue-3));
+}
+
+ul {
+  position: absolute;
+  top: calc(100% + 4px);
+  width: 100%;
+  padding: 1rem 1.5rem;
+  list-style-type: none;
+}
+
+ul li {
+  margin-block: 0.25rem;
 }
 </style>
