f193911466463c71ac5cf9d0d8e53e3c64681fc6
furxx2000 <furxx2000@gmail.com>
2022-10-02 13:16:58 +0800

    Call API to get countries data and using component to render it.

--- src/components/pages/CountryList.vue
+++ src/components/pages/CountryList.vue
@@ -1,13 +1,41 @@
 <script setup lang="ts">
+import { onBeforeMount, ref } from "@vue/runtime-core";
 import {} from "./CountryList.vue";
+
+const countries = ref(null);
+
+async function getCountry() {
+  try {
+    const url = "https://restcountries.com/v3.1/all";
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+
+    countries.value = data.slice(0, 10);
+  } catch (e) {
+    throw e;
+  }
+}
+
+onBeforeMount(() => {
+  getCountry();
+});
 </script>
 
 <template>
   <main class="grid">
     <BaseSearch />
     <BaseFilter />
-    <BaseCard />
-    <BaseCard />
+    <BaseCard
+      v-for="country in countries"
+      :key="country.name.official"
+      :country-flags="country.flags.svg"
+      :country-name="country.name.official"
+      :country-population="country.population"
+      :country-region="country.region"
+      :country-capital="country.capital"
+    />
   </main>
 </template>
 
