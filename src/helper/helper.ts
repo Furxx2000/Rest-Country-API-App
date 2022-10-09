70ed118aef700a2ec2a1a0fd3360fec779b37b98
furxx2000 <furxx2000@gmail.com>
2022-10-09 22:22:12 +0800

    Add logic and parameter to decide region

--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -1,9 +1,13 @@
 import {} from "./helper";
 
-export const AJAX = async (name: string | string[] | undefined = undefined) => {
-  const url = name
+export const AJAX = async (
+  name: string | string[] | undefined = undefined,
+  region: string | undefined = undefined
+) => {
+  let url = name
     ? `https://restcountries.com/v3.1/alpha/${name}`
     : "https://restcountries.com/v3.1/all";
+  url = region ? `https://restcountries.com/v3.1/region/${region}` : url;
   const res = await fetch(url);
   const data = await res.json();
 
