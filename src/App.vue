ece96427bf857fd0b59858a945143424730a00da
furxx2000 <furxx2000@gmail.com>
2022-10-10 17:32:06 +0800

    Save light and dark mode preference in local storage then set it

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,51 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
-import {} from "./main";
+import { onBeforeMount, ref } from "vue";
 
 const route = useRoute();
+const userTheme = ref();
+
+const getTheme = () => {
+  return localStorage.getItem("user-theme");
+};
+
+const setTheme = (userTheme: string) => {
+  localStorage.setItem("user-theme", userTheme);
+};
+
+const getMediaPreference = () => {
+  const hasDarkPreference = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  if (hasDarkPreference) return "dark-mode";
+  else return "light-mode";
+};
 
 const toggleMode = () => {
+  if (userTheme.value === "light-mode") {
+    setTheme("dark-mode");
+  } else {
+    setTheme("light-mode");
+  }
+  userTheme.value =
+    userTheme.value === "light-mode" ? "dark-mode" : "light-mode";
+  console.log(userTheme.value);
   document.querySelector("body")?.classList.toggle("dark-mode");
 };
+
+onBeforeMount(() => {
+  const theme = getTheme() || getMediaPreference();
+  userTheme.value = theme;
+
+  if (theme === "dark-mode")
+    document.querySelector("body")?.classList.add("dark-mode");
+  else document.querySelector("body")?.classList.remove("dark-mode");
+});
 </script>
 
 <template>
-  <TheHeader @mode="toggleMode" />
+  <TheHeader @mode="toggleMode" :user-theme="userTheme" />
+  <!-- <router-view></router-view> -->
   <router-view v-slot="{ Component }">
     <transition name="router" mode="out-in">
       <component :is="Component" :key="route.push"></component>
