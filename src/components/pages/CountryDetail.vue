b99a942fd5f4557d71c3d3fa12c58252f86e77de
furxx2000 <furxx2000@gmail.com>
2022-10-10 13:41:38 +0800

    Finish desktop style and RWD

--- src/components/pages/CountryDetail.vue
+++ src/components/pages/CountryDetail.vue
@@ -1,70 +1,3 @@
-<template>
-  <main class="grid">
-    <router-link to="/">
-      <button type="button" class="back-button fs-header">
-        <font-awesome-icon icon="fa-solid fa-arrow-left-long" />
-        Back
-      </button>
-    </router-link>
-
-    <article v-if="country" class="country grid">
-      <div class="country-flag">
-        <img :src="country.flag" />
-      </div>
-
-      <section class="country-detail grid">
-        <h3 class="fs-600 fw-800">{{ country.name }}</h3>
-        <ul class="flow">
-          <li>
-            Native Name: <span>{{ country.nativeName }}</span>
-          </li>
-          <li>
-            Population: <span>{{ country.population }}</span>
-          </li>
-          <li>
-            Region: <span>{{ country.region }}</span>
-          </li>
-          <li>
-            Sub Region: <span>{{ country.subRegion }}</span>
-          </li>
-          <li>
-            Capital: <span>{{ country.capital }}</span>
-          </li>
-        </ul>
-
-        <ul class="flow">
-          <li>
-            Top Level Domain: <span>{{ country.tld }}</span>
-          </li>
-          <li>
-            Currencies: <span>{{ country.currencies }}</span>
-          </li>
-          <li>
-            Languages:
-            <span v-for="lang in country.languages" :key="lang"
-              >{{ lang }},
-            </span>
-          </li>
-        </ul>
-
-        <div v-if="country.borders" class="country-border grid">
-          <h4 class="fs-400 fw-600">Border Countries:</h4>
-          <div class="borders flex">
-            <button
-              type="button"
-              @click="goToBorder(border.borderCca3)"
-              v-for="border in country.borders"
-              :key="border.borderName"
-            >
-              {{ border.borderName }}
-            </button>
-          </div>
-        </div>
-      </section>
-    </article>
-  </main>
-</template>
-
 <script setup lang="ts">
 import { watch } from "vue";
 import { ref } from "@vue/reactivity";
@@ -131,6 +64,77 @@ const goToBorder = (border: string) => {
 getCountry(name);
 </script>
 
+<template>
+  <main class="container grid">
+    <router-link to="/">
+      <button type="button" class="back-button fs-country-detail">
+        <font-awesome-icon icon="fa-solid fa-arrow-left-long" />
+        Back
+      </button>
+    </router-link>
+
+    <article v-if="country" class="country grid">
+      <div class="country-flag">
+        <img :src="country.flag" />
+      </div>
+
+      <section class="country-detail grid">
+        <h3 class="fs-600 fw-800">{{ country.name }}</h3>
+        <div class="details grid">
+          <ul class="flow">
+            <li>
+              Native Name: <span>{{ country.nativeName }}</span>
+            </li>
+            <li>
+              Population: <span>{{ country.population }}</span>
+            </li>
+            <li>
+              Region: <span>{{ country.region }}</span>
+            </li>
+            <li>
+              Sub Region: <span>{{ country.subRegion }}</span>
+            </li>
+            <li>
+              Capital: <span>{{ country.capital }}</span>
+            </li>
+          </ul>
+
+          <ul class="flow">
+            <li>
+              Top Level Domain: <span>{{ country.tld }}</span>
+            </li>
+            <li>
+              Currencies: <span>{{ country.currencies }}</span>
+            </li>
+            <li>
+              Languages:
+              <span v-for="lang in country.languages" :key="lang"
+                >{{ lang }},
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div v-if="country.borders" class="country-border">
+          <div class="flex">
+            <h4 class="fs-400 fw-600">Border Countries:</h4>
+            <div class="borders flex">
+              <button
+                type="button"
+                @click="goToBorder(border.borderCca3)"
+                v-for="border in country.borders"
+                :key="border.borderName"
+              >
+                {{ border.borderName }}
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </article>
+  </main>
+</template>
+
 <style scoped>
 .back-button {
   display: flex;
@@ -154,13 +158,19 @@ main {
 
 .country-flag {
   width: 100%;
-  height: 229px;
+  min-height: 229px;
   border-radius: 5.7px;
   background-color: gray;
+  box-shadow: 0px 0px 14px 4px rgba(0, 0, 0, 0.0294384);
+}
+
+.country-flag img {
+  border-radius: 5.7px;
 }
 
 .country-detail {
-  --gap: 1rem;
+  align-self: center;
+  --gap: 1.2rem;
 }
 
 .country-detail *:not(h3, h4) {
@@ -179,7 +189,7 @@ ul span {
   font-weight: var(--fw-300);
 }
 
-ul:nth-child(3) {
+ul:nth-child(2) {
   margin-block: 1rem;
 }
 
@@ -191,7 +201,54 @@ ul:nth-child(3) {
   box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.104931);
 }
 
+.country-border > div {
+  flex-direction: column;
+}
+
+.country-border h4 {
+  white-space: nowrap;
+}
+
 .borders {
   flex-wrap: wrap;
+  --gap: 10px;
+}
+
+@media only screen and (min-width: 1440px) {
+  main {
+    padding: 5rem 0;
+  }
+
+  .country {
+    grid-template-columns: 560px 574px;
+    justify-content: space-between;
+  }
+
+  .country-detail {
+    min-height: 323px;
+    --gap: 1.5rem;
+  }
+
+  .country-flag {
+    height: 401px;
+    border-radius: 10px;
+  }
+
+  .country-flag img {
+    border-radius: 10px;
+  }
+
+  .details {
+    grid-template-columns: 1fr 1fr;
+    align-items: flex-start;
+  }
+
+  ul:nth-child(2) {
+    margin-block: 0;
+  }
+
+  .country-border > div {
+    flex-direction: row;
+  }
 }
 </style>
