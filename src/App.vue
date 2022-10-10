d5507f566c2e19b89ed8a0935ef740b9ee225995
furxx2000 <furxx2000@gmail.com>
2022-10-10 16:09:54 +0800

    Add transition on router-view

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <script setup lang="ts">
+import { useRoute } from "vue-router";
 import {} from "./main";
 
-function toggleMode() {
+const route = useRoute();
+
+const toggleMode = () => {
   document.querySelector("body")?.classList.toggle("dark-mode");
-}
+};
 </script>
 
 <template>
   <TheHeader @mode="toggleMode" />
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="router" mode="out-in">
+      <component :is="Component" :key="route.push"></component>
+    </transition>
+  </router-view>
 </template>
 
 <style>
 body {
   background-color: hsl(var(--clr-light-gray));
   min-height: 100vh;
+}
+
+.router-enter-from {
+  opacity: 0;
+  translate: 0 -30px;
+}
+
+.router-leave-to {
+  opacity: 0;
+  translate: 0 30px;
+}
+
+.router-enter-active {
+  transition: all 0.5s ease-out;
+}
+
+.router-leave-active {
+  transition: all 0.5s ease-in;
+}
+
+.router-enter-to,
+.router-leave-from {
+  opacity: 1;
+  translate: 0 0;
 }
 </style>
